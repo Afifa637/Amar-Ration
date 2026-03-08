d399738df90a319359b4ece41afa953e04490845
Afifa Sultana <afifasultana637@gmail.com>
2026-03-09 04:19:04 +0600

    added db.js file and middleware error.js

--- backend/src/middleware/error.js
+++ backend/src/middleware/error.js
@@ -1,10 +1,8 @@
 function notFound(req, res) {
   res.status(404).json({ message: "Not Found" });
 }
-
 function errorHandler(err, req, res, next) {
   console.error(err);
   res.status(500).json({ message: "Server Error" });
 }
-
 module.exports = { notFound, errorHandler };
